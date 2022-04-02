d010ed061a9737204e07dd8451473ab0e6d89d86
David <er.email>
2022-04-02 18:01:55 -0500

    Added specialities, added to vet. closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
