7d50109d4ced8521a5292a538c0b9bb8087e5a5d
David <er.email>
2022-05-21 08:44:45 -0500

    added spring data jpa services for Pet. Closes #43

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -1,7 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
-import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetRepository extends CrudRepository<PetType, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> {
 }
