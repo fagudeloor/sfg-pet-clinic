583b2f1222b5b43d0f1271e73239f25db102f20f
David <er.email>
2022-05-21 09:38:16 -0500

    Implemented Map impl fpr Visit. Closes #46

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -18,7 +18,7 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
 
     @Override
     public Visit save(Visit visit) {
-        if (visit.getPet( == null || visit.getPet().getOwner() == null
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
                 || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null){
             throw new RuntimeException("Invalid Visit");
         }
